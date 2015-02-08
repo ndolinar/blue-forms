4453b30adf3564272b5e239d670e0b51ab45b1e7
Neja Dolinar <dolinar.neja@gmail.com>
2015-02-08 19:22:30 +0100

    Remove unnecessary assets.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
-
     $('input[type="checkbox"]').on('change', function(e){
         $(this).next('.checkbox_btn').toggleClass('checkbox_selected');
     });
 
     $('input[type="radio"]').on('change', function(e){
         $(this).closest('.option').siblings().find('.radio_btn').removeClass('radio_selected');
-
         $(this).next('.radio_btn').addClass('radio_selected');
     });
 
-    $(".flat-input-group > input").focus(function(e){
+    $('.flat-input-group > input').focus(function(e){
         $(this).parent().addClass('input-group-focus');
     }).blur(function(e){
             $(this).parent().removeClass('input-group-focus');
